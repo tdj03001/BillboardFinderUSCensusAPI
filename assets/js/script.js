ae5bdff3b13ad0f522f7801ab25974a56275d5c9
Tyler Jenkins <tyjenki@gmail.com>
2020-01-11 23:41:09 -0500

    cleaned out some Dev notes comments, added table IDs from index.html to jquery append code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,75 +10,33 @@ $(document).ready(function(){
     showTable();
   });
   
-  //because we're limiting the MVP version to just Philly, we are hard coding Philly's latitude and longitude in the "geometry" in the URL below
-  var settings = {
-    "url": `https://geoenrich.arcgis.com/arcgis/rest/services/World/geoenrichmentserver/GeoEnrichment/enrich?studyAreas=[
-    {
-      \"geometry\":{
-        \"x\":-75.165222,
-        \"y\":39.952583
-      }
-    }
-  ]&studyAreasOptions={
-    \"areaType\":\"RingBuffer\",
-    \"bufferUnits\":\"esriMiles\",
-    \"bufferRadii\":[1]
-  }&dataCollections=[\"KeyGlobalFacts\", \"KeyUSFacts\"]",
-    "method": "POST",
-    "timeout": 0,
-    "headers": {
-      "Content-Type": "application/x-www-form-urlencoded"
-    }`,
-    "data": {
-      "f": "json",
-      "token": "MWvelydDb3Ha4A_iT9k-bXgjFlIXG2JDN9NR7wjMpgEF9geal21mMaaOJkthcf-mZSeqyiiBpkQv1e4U4nON-4fbspXp20QN9OazqrcfkfmblmrD_owHh6_KtAGxikIOL_XtfHK40k2gF1wset9YGA..",
-      "inSR": "4326",
-      "outSR": "4326",
-      "returnGeometry": "true"
-    }
-  };
+  //hard coding Philly's county and state codes in the URL below. If business expands outside of Philadelphia County those codes would be dynamically generated with the user-input geography
   
-    $.ajax(settings).done(function(response) {
-      console.log(JSON.parse(response));
+  var queryURL="https://api.census.gov/data/2000/sf3?get=P001001,H001001,H018001,HCT012001,P082001,H076001,H063001&for=county:101&in=state:42";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    $("#totPopVal").append(response[1][0]); //Total pop.
+    $("#totHHVal").append(response[1][1]); //Total households
+    $("#avgHHSizeVal").append(response[1][2]); //Avg household size
+    $("#medHincVal").append(response[1][3]); //Median household income
+    $("#pciVal").append(response[1][4]); //Per capita income
+    $("#medValHousingVal").append(response[1][5]); //Median value of housing units
+    $("#grossRentVal").append(response[1][6]); //Median gross rent
     
-      $("#totPopVal").prepend(JSON.parse(response).results[0].value.FeatureSet[0].features[0].attributes.TOTPOP_CY);
-      $("#totMalesVal").prepend(JSON.parse(response).results[0].value.FeatureSet[0].features[0].attributes.TOTMALES);
-      $("#totFemalesVal").prepend(JSON.parse(response).results[0].value.FeatureSet[0].features[0].attributes.TOTFEMALES);
-      $("#totHHVal").prepend(JSON.parse(response).results[0].value.FeatureSet[0].features[0].attributes.TOTHH_CY);
-      $("#medHincVal").prepend(JSON.parse(response).results[0].value.FeatureSet[0].features[0].attributes.MEDHINC_CY);
-      $("#pciVal").prepend(JSON.parse(response).results[0].value.FeatureSet[0].features[0].attributes.PCI_CY);
-      $("#divIndxVal").prepend(JSON.parse(response).results[0].value.FeatureSet[0].features[0].attributes.DIVINDX_CY);
-    
-    
-    });
+  }); //closes ajax call
+
   }); //closes document.ready
   
   
     
 /*DEV NOTES====================================================
 
-DATA POINTS PULLED FROM API:
-  
-  FIELD NAME AND ID:
-  Total Population  id="totPop"
-  Total Male Population  id="totMales"
-  Total Female Population  id="totFemales"
-  2019 Total Households  id="totHH"
-  2019 Median Household Income  id="medHinc"
-  2019 Per Capita Income  id="pci"
-  2019 Diversity Index id="divIndx"
-  
-  DATA VALUE AND ID:
-  Total Population  id="totPopVal"
-  Total Male Population  id="totMalesVal"
-  Total Female Population  id="totFemalesVal"
-  2019 Total Households  id="totHHVal"
-  2019 Median Household Income  id="medHincVal"
-  2019 Per Capita Income  id="pciVal"
-  2019 Diversity Index id="divIndxVal"
-
-
   NOTES FOR FUTURE DEV:
-  - implement a demographics solution that doesn't require refreshing your token every 2 hours
+  - query more recent demographics
+  - arcGIS demographics has an expiring token every 2 hours on the free account, but it has more recent data
 
 ==============================================================*/
